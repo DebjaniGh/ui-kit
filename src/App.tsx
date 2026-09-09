94401ac5901ece2741a4cc53e7c88ff383a89978
Debjani <debjani6ghosh@gmail.com>
2026-09-09 13:19:46 +0530

    feat: refactor LoginPage to use AuthShell component and streamline layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
 import productIcon from "./assets/new_logo.svg";
 import "./App.css";
-// import { LoginPage, type Credentials } from "./templates/LoginPage/LoginPage";
-import {
-  RSALoginPage,
-  type RSACredentials,
-} from "./templates/RSALoginPage/RSALoginPage";
+import { LoginPage, type Credentials } from "./templates/LoginPage/LoginPage";
+// import {
+//   RSALoginPage,
+//   type RSACredentials,
+// } from "./templates/RSALoginPage/RSALoginPage";
 
 function App() {
-  // const selectOptions: { label: string; value: string }[] = [
-  //   { label: "This BMC UI", value: "bmc_ui" },
-  //   { label: "LDAP", value: "ldap" },
-  //   { label: "Active Directory", value: "active_dir" },
-  // ];
+  const selectOptions: { label: string; value: string }[] = [
+    { label: "This BMC UI", value: "bmc_ui" },
+    { label: "LDAP", value: "ldap" },
+    { label: "Active Directory", value: "active_dir" },
+  ];
 
-  // const links = [
-  //   { label: "Help", href: "https://www.dell.com/support/home" },
-  //   {
-  //     label: "Drivers & Downloads",
-  //     href: "https://www.dell.com/support/home/en-us?app=drivers",
-  //   },
-  //   {
-  //     label: "Manuals",
-  //     href: "https://www.dell.com/support/home/en-us?app=manuals",
-  //   },
-  //   { label: "TechCenter", href: "https://developer.dell.com" },
-  // ];
+  const links = [
+    { label: "Help", href: "https://www.dell.com/support/home" },
+    {
+      label: "Drivers & Downloads",
+      href: "https://www.dell.com/support/home/en-us?app=drivers",
+    },
+    {
+      label: "Manuals",
+      href: "https://www.dell.com/support/home/en-us?app=manuals",
+    },
+    { label: "TechCenter", href: "https://developer.dell.com" },
+  ];
 
-  // const handleLogin = (credentials: Credentials) => {
-  //   console.log(credentials);
-  // };
-
-  const handleRSALogin = (credentials: RSACredentials) => {
+  const handleLogin = (credentials: Credentials) => {
     console.log(credentials);
   };
 
+  // const handleRSALogin = (credentials: RSACredentials) => {
+  //   console.log(credentials);
+  // };
+
   return (
     <div>
-      {/* <LoginPage
+      <LoginPage
         productIcon={<img src={productIcon} alt="product icon" />}
         productTitle="Server BMC GUI"
         productSubtitle="Server Hostname | Server Model | License Type"
@@ -45,13 +45,13 @@ function App() {
         linksArray={links}
         miscellaneousMsg="Security Notice: Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
         "
-      /> */}
-      <RSALoginPage
+      />
+      {/* <RSALoginPage
         productIcon={<img src={productIcon} alt="product icon" />}
         productTitle="Server BMC GUI"
         onSubmit={handleRSALogin}
         miscellaneousMsg="Copyright Notice: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-      />
+      /> */}
     </div>
   );
 }
--- src/templates/LoginPage/LoginPage.tsx
+++ src/templates/LoginPage/LoginPage.tsx
@@ -1,10 +1,11 @@
 import { useState, type ReactNode } from "react";
-import dellLogo from "../../assets/logoipsum-288.png";
+
 import styles from "./LoginPage.module.css";
 import { Form } from "../../components/Form/Form";
 import { TextField } from "../../components/TextField/TextField";
 import { Button } from "../../components/Button/Button";
 import { SelectField } from "../../components/Select/SelectField";
+import { AuthShell } from "../AuthShell/AuthShell";
 
 export interface Credentials {
   username: string;
@@ -52,69 +53,39 @@ export function LoginPage({
 
   // JSX
   return (
-    <div className={styles.loginPage}>
-      <div className={styles.upperSection}>
-        {productIcon && <div className={styles.productIcon}>{productIcon}</div>}
-        <h1 className={styles.title}>{productTitle}</h1>
-        <div className={styles.logoContainer}>
-          <img
-            className={styles.companyLogo}
-            src={dellLogo}
-            alt="your company logo"
+    <AuthShell
+      productIcon={productIcon}
+      productTitle={productTitle}
+      productSubtitle={productSubtitle}
+      linksArray={linksArray}
+      miscellaneousMsg={miscellaneousMsg}
+    >
+      <Form onSubmit={handleSubmit}>
+        <TextField
+          label="Username"
+          value={usrname}
+          placeholder="Enter username here ..."
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          value={pwd}
+          placeholder="Enter password here ..."
+          onChange={(e) => setPwd(e.target.value)}
+        />
+        {domainOptions && domainOptions.length > 0 && (
+          <SelectField
+            label="Domain"
+            value={domain}
+            options={domainOptions}
+            onChange={(e) => setDomain(e.target.value)}
           />
-        </div>
-        {productSubtitle && (
-          <p className={styles.subtitle}>{productSubtitle}</p>
         )}
-      </div>
-      <div className={styles.inputArea}>
-        <Form onSubmit={handleSubmit}>
-          <TextField
-            label="Username"
-            value={usrname}
-            placeholder="Enter username here ..."
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            label="Password"
-            type="password"
-            value={pwd}
-            placeholder="Enter password here ..."
-            onChange={(e) => setPwd(e.target.value)}
-          />
-          {domainOptions && domainOptions.length > 0 && (
-            <SelectField
-              label="Domain"
-              value={domain}
-              options={domainOptions}
-              onChange={(e) => setDomain(e.target.value)}
-            />
-          )}
-          <div className={styles.loginBtn}>
-            <Button type="submit" label="Log in" />
-          </div>
-        </Form>
-      </div>
-      {linksArray && linksArray.length > 0 && (
-        <nav className={styles.links} aria-label="Helpful links">
-          {linksArray.map((link, index) => (
-            <span key={link.href}>
-              {index > 0 && <span className={styles.separator}>|</span>}
-              <a
-                className={styles.link}
-                href={link.href}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {link.label}
-              </a>
-            </span>
-          ))}
-        </nav>
-      )}
-      {miscellaneousMsg && (
-        <div className={styles.miscellaneous}>{miscellaneousMsg}</div>
-      )}
-    </div>
+        <div className={styles.loginBtn}>
+          <Button type="submit" label="Log in" />
+        </div>
+      </Form>
+    </AuthShell>
   );
 }
