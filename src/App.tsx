d6bcf6221b12a99d073b9cdb8ac75e9a710ceb8f
Debjani <debjani6ghosh@gmail.com>
2026-09-10 14:30:10 +0530

    feat: remove unused components and SVG assets from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,5 @@
-import { AppHeader } from "./components/AppHeader/AppHeader";
-import { IconButton } from "./components/IconButton/IconButton";
-import searchIcon from "./assets/search.svg";
-import userIcon from "./assets/user.svg";
-import helpIcon from "./assets/help-icon.svg";
-
 function App() {
-  return (
-    <div>
-      <AppHeader
-        productName="BMC UI Version 10 | License Type"
-        actions={
-          <>
-            <IconButton
-              icon={<img src={searchIcon} alt="" />}
-              ariaLabel="Search"
-              onClick={() => console.log("Search clicked")}
-            />
-            <IconButton
-              icon={<img src={userIcon} alt="" />}
-              ariaLabel="Lock"
-              onClick={() => console.log("Lock clicked")}
-            />
-            <IconButton
-              icon={<img src={helpIcon} alt="" />}
-              ariaLabel="Help"
-              onClick={() => console.log("Help clicked")}
-            />
-          </>
-        }
-      />
-      {/* rest of your dashboard content */}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
