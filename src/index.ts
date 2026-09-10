03a624b72c4e32935f31428b01d2ad0d8640f2fa
Debjani <debjani6ghosh@gmail.com>
2026-09-10 14:25:43 +0530

    feat: export AppHeader component from index

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export { Card } from "./components/Card/Card";
 export { StatusIndicator } from "./components/StatusIndicator/StatusIndicator";
 export { DropdownButton } from "./components/DropdownButton/DropdownButton";
 export { IconButton } from "./components/IconButton/IconButton";
+export { AppHeader } from "./components/AppHeader/AppHeader";
 export type { DropdownItem } from "./components/DropdownButton/DropdownButton";
 export type { Status } from "./components/StatusIndicator/StatusIndicator";
 export type { Credentials } from "./templates/LoginPage/LoginPage";
